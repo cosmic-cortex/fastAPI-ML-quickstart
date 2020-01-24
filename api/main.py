2b5cb295ff89d32f19b075b0f59e7cc19ba69d1d
cosmic-cortex <theodore.danka@gmail.com>
2020-01-24 09:22:23 +0100

    predict_csv endpoint added

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 
 from typing import List
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel, ValidationError, validator
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from .ml.model import load_model, n_features
 
@@ -11,7 +13,7 @@ from .ml.model import load_model, n_features
 class PredictRequest(BaseModel):
     data: List[List[float]]
 
-    @validator('data')
+    @validator("data")
     def check_dimensionality(cls, v):
         for point in v:
             if len(point) != n_features:
@@ -28,15 +30,27 @@ app = FastAPI()
 model = load_model()
 
 
-@app.post('/predict', response_model=PredictResponse)
+@app.post("/predict", response_model=PredictResponse)
 def predict(input: PredictRequest):
     X = np.array(input.data)
     y_pred = model.predict(X)
     result = PredictResponse(data=y_pred.tolist())
+
     return result
 
 
-@app.post('/predict-csv')
+@app.post("/predict_csv")
 def predict_csv(csv_file: UploadFile = File(...)):
-    import pdb
-    pdb.set_trace()
+    df = pd.read_csv(csv_file.file)
+
+    df_n_instances, df_n_features = df.shape
+    if df_n_features != n_features:
+        raise HTTPException(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Each data point must contain {n_features} features",
+        )
+
+    y_pred = model.predict(df.to_numpy().reshape(-1, n_features))
+    result = PredictResponse(data=y_pred.tolist())
+
+    return result
--- api/tests/api/test_api.py
+++ api/tests/api/test_api.py
@@ -1,17 +1,47 @@
 import pytest
 import random
-from starlette.testclient import TestClient
-from fastapi.encoders import jsonable_encoder
+import tempfile
+from pathlib import Path
+from itertools import product
 
-from api.main import PredictRequest
+from starlette.testclient import TestClient
+from starlette.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
+
 from api.ml.model import n_features
+
+
+n_instances_range = range(1, 10)
 
 
 @pytest.mark.parametrize("n_instances", range(1, 10))
 def test_predict(n_instances: int, test_client: TestClient):
-    fake_data = [[random.random() for _ in range(n_features)]
-                 for _ in range(n_instances)]
-    json_data = jsonable_encoder(PredictRequest(data=fake_data))
-    response = test_client.post('/predict', json=json_data)
-    assert response.status_code == 200
-    assert len(response.json()['data']) == n_instances
+    fake_data = [[random.random() for _ in range(n_features)] for _ in range(n_instances)]
+    response = test_client.post("/predict", json={"data": fake_data})
+    assert response.status_code == HTTP_200_OK
+    assert len(response.json()["data"]) == n_instances
+
+
+@pytest.mark.parametrize(
+    "n_instances, test_data_n_features",
+    product(n_instances_range, [n for n in range(1, 20) if n != n_features]),
+)
+def test_predict_with_wrong_input(
+    n_instances: int, test_data_n_features: int, test_client: TestClient
+):
+    fake_data = [[random.random() for _ in range(test_data_n_features)] for _ in range(n_instances)]
+    response = test_client.post("/predict", json={"data": fake_data})
+    assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_predict_csv(test_client: TestClient):
+    data_path = Path(__file__).parent / "data_correct.csv"
+    with open(data_path, "r") as csv_file:
+        response = test_client.post("/predict_csv", files={"csv_file": csv_file})
+        assert response.status_code == HTTP_200_OK
+
+
+def test_predict_csv_with_wrong_input(test_client: TestClient):
+    data_path = Path(__file__).parent / "data_incorrect.csv"
+    with open(data_path, "r") as csv_file:
+        response = test_client.post("/predict_csv", files={"csv_file": csv_file})
+        assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
