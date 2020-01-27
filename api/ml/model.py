925cb80252afe452a095f1aa9aeb990292202f0c
cosmic-cortex <theodore.danka@gmail.com>
2020-01-27 13:37:19 +0100

    Model class attributes changed to private, train and load returns self instead of the model object

--- api/ml/model.py
+++ api/ml/model.py
@@ -12,25 +12,26 @@ n_features = load_boston(return_X_y=True)[0].shape[1]
 
 class Model:
     def __init__(self, model_path: str = None):
-        self.model = None
-        self.model_path = model_path
+        self._model = None
+        self._model_path = model_path
 
     def train(self, X: np.ndarray, y: np.ndarray):
-        self.model = RandomForestRegressor()
-        self.model.fit(X, y)
-        return self.model
+        self._model = RandomForestRegressor()
+        self._model.fit(X, y)
+        return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        return self.model.predict(X)
+        return self._model.predict(X)
 
     def save(self):
-        if self.model is not None:
-            joblib.dump(self.model, self.model_path)
+        if self._model is not None:
+            joblib.dump(self._model, self._model_path)
         else:
             raise TypeError("The model is not trained yet, use .train() before saving")
 
     def load(self):
-        self.model = joblib.load(self.model_path)
+        self._model = joblib.load(self._model_path)
+        return self
 
 
 model = Model(model_path)
